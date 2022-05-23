88ff5bb2108ff651b16857bb2e7fc4eabcb25dcc
ShihabReza <shihabreza457@gmail.com>
2022-05-23 22:02:11 +0600

    add-server

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
 app.use(cors())
 const port = process.env.PORT || 5000
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mpzy2.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const productCollection = client.db('pix_parts').collection('products');
+      app.get('/product',async(req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const product = await cursor.toArray()
+            res.send(product)
+      })
+    } finally {
+     
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('pic-parts-server')
